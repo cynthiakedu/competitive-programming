297c71ac8dbb893b85b157f2077be62aaedbe67b
Cynthia <cynthiad@mit.edu>
2020-11-02 08:14:24 -0500

    First commit

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+const int MXN = 100005;
+
+
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(0);
+    cout << "Hello world! " << endl;
+
+
+    return 0;
+}
