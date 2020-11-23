455b88c0e6294736273e7b447698f0ba9d375749
Cynthia <cynthiad@mit.edu>
2020-11-23 10:14:09 -0500

    Atcoder B184 C

--- atcoder/B184/C.cpp
+++ atcoder/B184/C.cpp
@@ -8,18 +8,15 @@ int main() {
     cin >> r1 >> c1 >> r2 >> c2;
 
     int x = abs(r1 - r2), y = abs(c1 - c2);
-    if (x == 0 && y == 0) {
-        cout << "0\n";
+    int ans = 3;
+    if (x + y == 0) {
+        ans = 0;
     } else if (x + y <= 3 || x == y) {
-        cout << "1\n";
-    } else if ((x + y) % 2 == 0) {
-        cout << "2\n";
-    } else {
-        if (y < x) swap(x, y);
-        int ans = ((x > 0 ? 1 : 0) + (y - x <= 3 ? (y - x == 0 ? 0 : 1) : ((y - x) % 2 ? 3 : 2)));
-        ans = min(ans, 3);
-        cout << ans << "\n";
+        ans = 1;
+    } else if (abs(x - y) <= 6 || (x + y) % 2 == 0 || abs(x - y) == 3) {
+        ans = 2;
     }
+    cout << ans << "\n";
 
     return 0;
 }
